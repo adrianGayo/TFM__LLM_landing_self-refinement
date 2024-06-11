3ad6fcfe23ddab29489c7f1867b878b54e83ec26
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-06-11 14:19:07 +0200

    Commit con 3.5 de prueba

--- Action.py
+++ Action.py
@@ -1,8 +1,26 @@
+import random
+
 def act(observation):
-    x_vel = observation[4]
-    if x_vel > 0.5:
-        return 1
-    elif x_vel < -0.5:
-        return 3
-    else:
-        return 2
+    '''
+    The function that codifies the action to be taken in each instant of time.
+
+    Args:
+        observation (numpy.array):
+            "description": "The state of the environment after the action is taken.",
+            "positions": {  
+                "0": "X position",
+                "1": "Y position",
+                "2": "X velocity",
+                "3": "Y velocity",
+                "4": "Angle",
+                "5": "Angular velocity",
+                "6": "Left contact sensor",
+                "7": "Right contact sensor"
+            },
+            "min_values": [-1.5, -1.5, -5.0, -5.0, -3.14, -5.0, 0, 0],
+            "max_values": [1.5, 1.5, 5.0, 5.0, 3.14, 5.0, 1, 1]
+
+    Returns:
+        Integer  : The action to be taken.
+    '''
+    return random.randint(0, 3)
