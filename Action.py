6d05b412fc422c867646906d1e59ff145d7275e2
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-06-11 19:49:09 +0200

    Añadir vídeo recoder

--- Action.py
+++ Action.py
@@ -1,4 +1,4 @@
-import numpy as np
+import random
 
 def act(observation):
     '''
@@ -7,7 +7,7 @@ def act(observation):
     Args:
         observation (numpy.array):
             "description": "The state of the environment after the action is taken.",
-            "positions": {
+            "positions": {  
                 "0": "X position",
                 "1": "Y position",
                 "2": "X velocity",
@@ -23,33 +23,4 @@ def act(observation):
     Returns:
         Integer  : The action to be taken.
     '''
-    x_pos, y_pos, x_vel, y_vel, angle, angular_vel, left_contact, right_contact = observation
-
-    THRESHOLD_ANGLE = 0.1
-    THRESHOLD_VELOCITY_X = 0.2
-    THRESHOLD_VELOCITY_Y = -0.5
-    HOVER_HEIGHT = 0.6
-    
-    if left_contact or right_contact:
-        return 3
-
-    # More aggressive control for managing descent and stability
-    if abs(angle) > THRESHOLD_ANGLE / 2:
-        if angle > 0:
-            return 0
-        else:
-            return 1
-
-    if abs(x_vel) > THRESHOLD_VELOCITY_X / 2:
-        if x_vel > 0:
-            return 0
-        else:
-            return 1
-
-    if y_pos > HOVER_HEIGHT * 1.5 and y_vel > 1.5 * THRESHOLD_VELOCITY_Y:
-        return 3
-
-    if y_vel < THRESHOLD_VELOCITY_Y or y_pos > 0.3:
-        return 2
-
-    return 3
+    return random.randint(0, 3)
