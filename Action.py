d14ecd506cb5fec607888e2aaf5c108f23e99e39
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-06-18 17:24:06 +0200

    Nuevo prompt. Iteración 2.

--- Action.py
+++ Action.py
@@ -1,6 +1,9 @@
 def act(observation):
     x_position, y_position, x_velocity, y_velocity, angle, angular_velocity, left_contact, right_contact = observation
     
+    if left_contact == 1 or right_contact == 1:  # Successful landing
+        return 0
+
     # Stabilize angle
     if angle < -0.1:
         return 3  # fire right engine
@@ -8,13 +11,13 @@ def act(observation):
         return 1  # fire left engine
 
     # Stabilize X speed if too high
-    if x_velocity > 0.5:
+    if x_velocity > 0.3:
         return 3  # fire right engine
-    elif x_velocity < -0.5:
+    elif x_velocity < -0.3:
         return 1  # fire left engine
 
     # If descending too fast
-    if y_velocity < -0.5:
+    if y_velocity < -0.3:
         return 2  # fire main engine to slow down descent
 
     # If within safe parameters, turn off engines
