0b5f0c17833ef527767e7554ef7ee675e2b46b38
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-06-19 12:36:30 +0200

    Nuevo prompt. GPT3.5. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,35 +1,25 @@
-def act(observation):
-    # Extract relevant information from the observation
-    speed_x = observation[0]
-    speed_y = observation[1]
-    pos_x = observation[2]
-    pos_y = observation[3]
-    angle = observation[4]
-    base_action = 0
-    side_engine_penalty = 0.03
-    center_engine_penalty = 0.3
-    score = 0
-
-    # Implement the decision-making process
-    if pos_y > 0.5:
-        if angle > 0.05 or angle < -0.05:
-            base_action = 3  # Rotate to stabilize
-            score -= center_engine_penalty
-        elif pos_y > 1:
-            if speed_y < -0.1:
-                base_action = 1  # Slow down the descent
-                score -= side_engine_penalty
-        else:
-            if abs(speed_x) > 0.05:
-                base_action = 1  # Correct horizontal speed
-                score -= side_engine_penalty
-        score -= side_engine_penalty  # Continuous stabilization
+def act(data):
+    
+    acc_x = data[2]
+    acc_y = data[3]
+    vel_x = data[4]
+    vel_y = data[5]
+    landing_status = data[1]
+    score = data[6]
+    action = 0
+    
+    # Implement your decision-making process here
+    if landing_status == 3:  # If the landing is out of bounds
+        action = 0
+    elif acc_y < -0.2:  # Increase altitude if falling too fast
+        action = 1
+    elif acc_y > 0.2:  # Decrease altitude if ascending too fast
+        action = 3
+    elif vel_x > 0.05:  # Move left
+        action = 2
+    elif vel_x < -0.05:  # Move right
+        action = 4
     else:
-        if abs(speed_x) > 0.01:
-            base_action = 1  # Correct horizontal speed
-            score -= side_engine_penalty
-        if angle > 0.05 or angle < -0.05:
-            base_action = 3  # Rotate to stabilize
-            score -= center_engine_penalty
-
-    return base_action, score
+        action = 0  # Stay still
+    
+    return action
