a47f8c02cfaff9b58fede91c29b770dd78437980
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-02 01:32:26 +0200

    4o. Iteración 3.

--- Action.py
+++ Action.py
@@ -1,20 +1,20 @@
 def act(observation):
     x_pos, y_pos, x_vel, y_vel, angle, angular_vel, left_contact, right_contact = observation
-    
-    # Prioritize stabilization
+
+    # First priority: Stabilize the spacecraft angle
     if angle < -0.1 or angular_vel < -0.5:  # Too tilted to the left, apply right thrust
         return 1
     elif angle > 0.1 or angular_vel > 0.5:  # Too tilted to the right, apply left thrust
         return 3
-    
-    # Manage horizontal position and velocity
+
+    # Second priority: Manage horizontal position and velocity
     if x_pos < -0.1 or x_vel < -0.5:  # Too far left, apply right thrust
         return 3
     elif x_pos > 0.1 or x_vel > 0.5:  # Too far right, apply left thrust
         return 1
 
-    # Control descent speed
-    if y_vel < -0.5:  # Falling too fast, apply main engine
+    # Third priority: Control descent speed
+    if y_vel < -0.2:  # Any significant downward velocity needs correction
         return 2
 
     # Default action if all parameters are in acceptable range
