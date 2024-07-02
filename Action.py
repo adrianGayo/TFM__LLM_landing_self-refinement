0c0254985ff1f85dea7ebd6705acd98589f2101c
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-02 18:16:03 +0200

    4o JSONMODE. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,11 +1,7 @@
-def act(curr_status):
-    x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = curr_status
-    if left_contact == 0 and right_contact == 0:
-        if x_pos < -0.1:
-            return 3  # Push right engine
-        elif x_pos > 0.1:
-            return 1  # Push left engine
-        else:
-            return 2  # Push both engines (upwards)
-    else:
+def act(data):
+    if data['logs'][-1]['current status'][0] > 0:
         return 0  # Switch off engines
+    else:
+        return 1  # Push left engine
+
+# You may further customize this function to consider other factors influencing a successful landing.
