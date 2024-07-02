c97af7cddf80c9a69c223fbe15a65f02306b4305
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-02 14:06:02 +0200

    4o Setpback. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,23 +1,22 @@
-def act(observation):
-    x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation
-    
-    # Correct horizontal position if far from center
-    if abs(x_pos) > 0.1:
-        if x_pos > 0:
-            return 1  # Push left engine to move left
-        else:
-            return 3  # Push right engine to move right
-    
-    # Correct angle if too tilted
-    if abs(angle) > 0.1:
-        if angle > 0:
-            return 3  # Push right engine to tilt left
-        else:
-            return 1  # Push left engine to tilt right
-    
-    # Correct vertical velocity
-    if y_vel < -0.2:
-        return 2  # Push both engines upwards to decelerate
-    
-    # If all conditions are satisfactory, switch off engines
-    return 0
+import random 
+
+def act(observation): 
+  x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation[0], observation[1], observation[2], observation[3], observation[4], observation[5], observation[6], observation[7] 
+  
+  if left_contact or right_contact:  # touchdown event if any contact 
+    return 0  # switch off engines 
+  
+  if x_vel > 0.1:  # move left
+    return 1  # left engine 
+  elif x_vel < -0.1:  # move right
+    return 3  # right engine 
+  
+  if y_vel < -0.1:  # adjust descent
+    return 2  # both engines up 
+  
+  if angle < -0.1:  # clockwise rotate
+    return 3  # right engine 
+  elif angle > 0.1:  # counter-clockwise rotate
+    return 1  # left engine 
+  
+  return random.randint(0, 3)  # default, small random adjustments 
