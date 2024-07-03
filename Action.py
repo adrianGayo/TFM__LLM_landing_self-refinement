a1bab8f55c46516fe8cb8d161f9a87b14ec8cef2
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-03 12:48:59 +0200

    4o genetic. Iteración 4.

--- Action.py
+++ Action.py
@@ -23,29 +23,24 @@ def act(observation):
 
     Args:
         observation (numpy.array):
-            "description": "The state of the environment after the action is taken.",
-            "positions": {  
-                "0": "X position",
-                "1": "Y position",
-                "2": "X velocity",
-                "3": "Y velocity",
-                "4": "Angle",
-                "5": "Angular velocity",
-                "6": "Left contact sensor",
-                "7": "Right contact sensor"
-            },
-            "min_values": [-1.5, -1.5, -5.0, -5.0, -3.14, -5.0, 0, 0],
-            "max_values": [1.5, 1.5, 5.0, 5.0, 3.14, 5.0, 1, 1]
+            Description: The state of the environment after the action is taken.
+            Positions: {  
+                0: X position,
+                1: Y position,
+                2: X velocity,
+                3: Y velocity,
+                4: Angle,
+                5: Angular velocity,
+                6: Left contact sensor,
+                7: Right contact sensor
+            }
+            Min_values: [-1.5, -1.5, -5.0, -5.0, -3.14, -5.0, 0, 0],
+            Max_values: [1.5, 1.5, 5.0, 5.0, 3.14, 5.0, 1, 1]
 
     Returns:
-        Integer : The action to be taken.
-        "options": {
-                '0' : "Switch off engines",
-                '1' : "Push left engine",
-                '2' : "Push both engines (upwards)",
-                '3' : "Push right engine"
-            }
+        Integer: The action to be taken.
     '''
+
     x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation
 
     # Check for successful landing
