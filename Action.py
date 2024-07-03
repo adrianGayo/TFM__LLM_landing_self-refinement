5e9ad03e8639bce70816e2328a234109dc3d6d44
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-03 11:53:01 +0200

    4o genetic. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,10 +1,27 @@
-def act(env_logs):
-    for log in env_logs:
-        if log['current status'][0] < 0:
-            return 3  # Fire right engine
-        elif log['current status'][0] > 0:
-            return 1  # Fire left engine
-        elif log['current status'][3] > -0.35:
-            return 2  # Fire both engines (upwards)
-        else:
-            return 0  # Switch off engines
+def act(observation):
+    # Unpacking observation elements
+    x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation
+    
+    # Control strategy based on observations
+    # Control angle and angular velocity first
+    if angle > 0.1:  # If tilted to the right
+        return 1  # Fire left engine to rotate left
+    elif angle < -0.1:  # If tilted to the left
+        return 3  # Fire right engine to rotate right
+    elif ang_vel > 0.5:  # If rotating clockwise fast
+        return 1  # Fire left engine to reduce rotation to left
+    elif ang_vel < -0.5:  # If rotating counter-clockwise fast
+        return 3  # Fire right engine to reduce rotation to right
+    
+    # Control horizontal velocity next
+    if x_vel > 0.5:  # Moving too fast to the right
+        return 1  # Fire left engine to reduce horizontal velocity to right
+    elif x_vel < -0.5:  # Moving too fast to the left
+        return 3  # Fire right engine to reduce horizontal velocity to left
+    
+    # Finally, control vertical descent
+    if y_vel < -1.0:  # Descending too fast
+        return 2  # Fire both engines to slow descent
+    
+    # Default action is to switch off engines to preserve fuel (scores)
+    return 0
