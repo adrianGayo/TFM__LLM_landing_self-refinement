467c6b75c692224d61dd9b655dcb25b0c996eb03
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-04 15:27:15 +0200

    4o genetic FC. Iteración 2.

--- Action.py
+++ Action.py
@@ -1,31 +1,18 @@
+import numpy as np
+
 def act(observation):
     x_position, y_position, x_velocity, y_velocity, angle, angular_velocity, left_contact, right_contact = observation
 
-    # thresholds for control
+    # Thresholds for control
     vertical_speed_threshold = -0.3
     horizontal_speed_threshold = 0.1
     angle_threshold = 0.1
-    critical_angle_threshold = 0.5
-    critical_speed_threshold = -2.0
 
-    # Extreme conditions
-    if y_velocity < critical_speed_threshold or abs(angle) > critical_angle_threshold:
-        return 2  # fire both engines to stabilize
-    
-    # Correcting vertical speed
-    if y_velocity < vertical_speed_threshold:
-        return 2  # fire both engines to slow descent
-    
-    # Correcting horizontal speed
-    if x_velocity > horizontal_speed_threshold:
-        return 1  # fire left engine to reduce rightward velocity
-    elif x_velocity < -horizontal_speed_threshold:
-        return 3  # fire right engine to reduce leftward velocity
-    
-    # Correcting angles
-    if angle < -angle_threshold:
-        return 1  # fire left engine to correct angle left
+    if y_velocity < vertical_speed_threshold or abs(x_velocity) > horizontal_speed_threshold:
+        return 2  # Fire both engines
+    elif angle < -angle_threshold:
+        return 1  # Fire the left engine to correct the angle
     elif angle > angle_threshold:
-        return 3  # fire right engine to correct angle right
-    
-    return 0  # switch off engines when stable
+        return 3  # Fire the right engine to correct the angle
+    else:
+        return 0  # Switch off engines
