8c996fe6804325e02b5cdbe38512d3ee97106284
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-04 17:51:04 +0200

    4o genetic JSON. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,32 +1,26 @@
-import numpy as np
-
 def act(observation):
     x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation
-    
-    if right_contact or left_contact:
-        return 0
 
-    # Reduce vertical speed if falling too fast
+    # Dictionary for possible actions
+    actions = {'off': 0, 'left': 1, 'up': 2, 'right': 3}
+
+    # Prioritize reducing y_velocity and x_velocity
+    action = actions['off']
+
+    # If falling too quickly, engage the main engine
     if y_vel < -0.5:
-        return 2
-    
-    # Align horizontally to the landing zone
-    if x_pos > 0.1:
-        return 1
-    elif x_pos < -0.1:
-        return 3
-    
-    # Correct the angle to keep the spacecraft upright
-    if angle > 0.1:
-        return 1
-    elif angle < -0.1:
-        return 3
-    
-    # Reduce angular velocity if rotating too much
-    if ang_vel > 0.2:
-        return 1
-    elif ang_vel < -0.2:
-        return 3
+        action = actions['up']
 
-    # Switch off engines if relatively stable
-    return 0
+    # Adjust horizontal movements
+    elif x_vel > 0.1:   # Moving right
+        action = actions['left']
+    elif x_vel < -0.1:  # Moving left
+        action = actions['right']
+
+    # Correct angular position
+    if angle > 0.1:      # Tilted right
+        action = actions['left']
+    elif angle < -0.1:   # Tilted left
+        action = actions['right']
+
+    return action
