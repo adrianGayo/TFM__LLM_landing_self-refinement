4170490eabe107651890c5e211055d468c4966c6
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-08 16:56:59 +0200

    4o genetic JSON. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,19 +1,27 @@
-def act(observation):
-    x_position, y_position, x_velocity, y_velocity, angle, angular_velocity, left_contact, right_contact = observation
-    threshold = 0.05
-    if abs(x_velocity) > threshold:
-        if x_velocity > 0:
-            return 1  # Push left engine to counteract x_velocity to right
-        else:
-            return 3  # Push right engine to counteract x_velocity to left
-    
-    if abs(angle) > threshold:
-        if angle > 0:
-            return 1  # Push left engine to counteract positive tilt
-        else:
-            return 3  # Push right engine to counteract negative tilt
+import numpy as np
 
-    if y_velocity < -threshold or y_position > 0.1:
-        return 2  # Push upward to slow descent
+def act(observation):
+    x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation
     
-    return 0  # If all within thresholds, switch off engines
+    # If we have landed, turn off engines
+    if left_contact == 1 and right_contact == 1:
+        return 0
+    
+    # Correct horizontal velocity
+    if x_vel > 0.1:
+        return 1  # push left engine to move left
+    elif x_vel < -0.1:
+        return 3  # push right engine to move right
+    
+    # Correct angle to upright
+    if angle > 0.1:
+        return 1  # tilt left using left engine
+    elif angle < -0.1:
+        return 3  # tilt right using right engine
+    
+    # Use central engine to control descent speed
+    if y_vel < -0.5:
+        return 2  # push central engine to slow down
+    
+    # If everything is nominal, turn off engines
+    return 0
