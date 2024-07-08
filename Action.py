8f4c00b07aa27d93943e00527d405c505de607d0
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-08 14:30:26 +0200

    4o Setpback. Iteración 2.

--- Action.py
+++ Action.py
@@ -14,19 +14,26 @@ def act(observation):
     if y_vel < -MAX_VERTICAL_SPEED:
         return 2
     
-    # Use left/right engines to correct horizontal speed or tilt
-    if abs(angle) > MAX_ANGLE or abs(x_vel) > MAX_HORIZONTAL_SPEED:
-        if angle > 0 or x_vel > 0:
+    # Correct horizontal velocity
+    if abs(x_vel) > MAX_HORIZONTAL_SPEED:
+        if x_vel > 0:
+            return 3  # Push right engine
+        else:
+            return 1  # Push left engine
+    
+    # Correct angle
+    if abs(angle) > MAX_ANGLE:
+        if angle > 0:
             return 1  # Push left engine
         else:
             return 3  # Push right engine
     
-    # Limit angular velocity
+    # Correct angular velocity
     if abs(ang_vel) > MAX_ANGULAR_VELOCITY:
         if ang_vel > 0:
             return 1  # Push left engine
         else:
             return 3  # Push right engine
     
-    # Otherwise, switch off engines
+    # Default action: switch off engines
     return 0
