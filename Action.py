c7803cb42e08b4650c16b4d5c4624434bdf07c28
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-09 00:35:24 +0200

    4o genetic JSON. Iteración 1.

--- Action.py
+++ Action.py
@@ -1,27 +1,51 @@
-import numpy as np
+'import random
 
-def act(observation):
-    x_pos, y_pos, x_vel, y_vel, angle, angular_vel, left_contact, right_contact = observation
-
-    # Define thresholds based on successful landings
-    stop_y_threshold = -0.3  # Threshold for acceptable y velocity
-    tilt_threshold = 0.1  # Threshold for acceptable tilt
-
-    # Use upward engines to slow down descent
-    if y_vel < stop_y_threshold:
-        return 2
-    
-    # Control horizontal velocity based on x position
-    if x_pos > 0.1:
-        return 1  # Push left to correct right drift
-    elif x_pos < -0.1:
-        return 3  # Push right to correct left drift
-
-    # Control tilt by counter-acting angle with side engines
-    if angle > tilt_threshold:
-        return 1  # Push left to correct right tilt
-    elif angle < -tilt_threshold:
-        return 3  # Push right to correct left tilt
-
-    # In the default case, turn off all engines
-    return 0
+ def act(observation):
+ ''' The function that codifies the action to be taken in each instant of time. 
+ 
+ Args: 
+ observation (numpy.array): 
+ "description": "The state of the environment after the action is taken.", 
+ "positions": { 
+ "0": "X position", 
+ "1": "Y position", 
+ "2": "X velocity", 
+ "3": "Y velocity", 
+ "4": "Angle", 
+ "5": "Angular velocity", 
+ "6": "Left contact sensor", 
+ "7": "Right contact sensor" 
+ }, 
+ "min_values": [-1.5, -1.5, -5.0, -5.0, -3.14, -5.0, 0, 0], 
+ "max_values": [1.5, 1.5, 5.0, 5.0, 3.14, 5.0, 1, 1] 
+ 
+ Returns: 
+ Integer : The action to be taken. 
+ "options": { 
+ '0' : "Switch off engines", 
+ '1' : "Push left engine", 
+ '2' : "Push both engines (upwards)", 
+ '3' : "Push right engine" 
+ } 
+ ''' 
+ 
+ x_pos, y_pos, x_vel, y_vel, angle, ang_vel, left_contact, right_contact = observation 
+ 
+ # Thrust upwards if falling too fast 
+ if y_vel < -0.3: 
+ return 2 
+ 
+ # Adjust horizontal position using left and right engines 
+ if x_pos > 0.1: 
+ return 1 
+ elif x_pos < -0.1: 
+ return 3 
+ 
+ # Control angle if tilted too much 
+ if angle > 0.1: 
+ return 1 
+ elif angle < -0.1: 
+ return 3 
+ 
+ # Switch off engines to conserve fuel in stable conditions 
+ return 0'
