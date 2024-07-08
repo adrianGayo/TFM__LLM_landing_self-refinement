df09515fc5cccdb74c9a04c5a33b2ee4ed190d0b
adrianGayo <adrian.gayo@alumnos.upm.es>
2024-07-08 13:59:09 +0200

    4o JSONMODE. Iteración 2.

--- Action.py
+++ Action.py
@@ -16,18 +16,15 @@ def act(log_entry):
         if left_contact == 1 and right_contact == 1:
             action = 0
         else:
-            # Control descent speed
             if y_vel < -0.5:
                 action = 2  # Engage both engines to slow descent
 
-            # Adjust horizontal position
-            if x_pos < -0.1 and x_vel < 0.1:
+            elif x_pos < -0.1 and x_vel < 0.1:
                 action = 3  # Push right engine to move left
             elif x_pos > 0.1 and x_vel > -0.1:
                 action = 1  # Push left engine to move right
 
-            # Adjust tilt
-            if angle < -0.1 and ang_vel < 0.1:
+            elif angle < -0.1 and ang_vel < 0.1:
                 action = 3  # Push right engine to correct left tilt
             elif angle > 0.1 and ang_vel > -0.1:
                 action = 1  # Push left engine to correct right tilt
